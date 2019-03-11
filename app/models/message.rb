e9de50f87134a60933616829358e099ed2d5a5d4
pattywitwhy <pattyxhwang@gmail.com>
2019-03-11 12:41:04 -0500

    translstion medthod in messages model added and working

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,41 +3,33 @@ class Message < ApplicationRecord
   belongs_to :user
   has_many :conversations
 
-  def initialize(input_options)
-    super
-    translate
-  end
-
   def translate
+
     translations = {
                      "mom" => "엄마",
                      "dad" => "아빠",
-                     "older_sister" => "언니",
-                     "little_sister" => "동생",
-                     "car" => "차",
-                     "dinner" => "전역",
-                     "lunch" => "전심",
+                     "Rosa" => "언니",
+                     "Annie" => "애니",
                      "breakfast" => "아침",
+                     "lunch" => "전심",
+                     "dinner" => "전역",
                      "work" => "일",
-                     "why" => "애"
-                  }
+                     "why" => "왜",
+                     "no" => "아니",
+                     "again" => "또",
+                     "i don't want to" => "씨러",
+                     "what are we eating" => "뭐 먹어",
+                     "mom where are you" => "엄마 디야?",
+                     "dad where are you" => "아빠 어디야",
+                     "when are you coming" => "언재와",
+                     "where are you going" => "어디가",
+                     "what is it" => "뭐야",
+                     "i don't know" => "몰리"
+                    }
 
-    self.each_with_index do |str, ind|
-      translations.each_key do |word|
-        self[ind].gsub!(word, translations[word]) if str.include?(word)
-      end
+    translations.each_key do |word|
+      body.gsub!(word, translations[word]) if body.include?(word)
     end
-    
   end
-
-  # message = ["mom what are you doing", "dad what are you doing", "mom and dad", "what's for lunch"]
-
-
-  # message.each_with_index do |str,ind|
-  #   translations.each_key do |word|
-  #     message[ind].gsub!(word,translations[word]) if str.include?(word)
-  #   end
-  # end
-
 
 end
