0866a442cbbbfc1d9daf1d3784d669b12b523631
pattywitwhy <pattyxhwang@gmail.com>
2019-03-14 18:04:01 -0500

    messages model changed

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,6 +8,15 @@ class Message < ApplicationRecord
 
   def translate
     translations = {
+                     "i don't want to" => "씨러",
+                     "what are we eating" => "뭐 먹어",
+                     "mom where are you" => "엄마 어디야?",
+                     "dad where are you" => "아빠 어디야",
+                     "when are you coming" => "언재와",
+                     "where are you going" => "어디가",
+                     "what are you doing" => "뭐해",
+                     "what is it" => "뭐야",
+                     "i don't know" => "몰리",
                      "mom" => "엄마",
                      "dad" => "아빠",
                      "Rosa" => "언니",
@@ -18,15 +27,6 @@ class Message < ApplicationRecord
                      "work" => "일",
                      "why" => "왜",
                      "again" => "또",
-                     "i don't want to" => "씨러",
-                     "what are we eating" => "뭐 먹어",
-                     "mom where are you" => "엄마 디야?",
-                     "dad where are you" => "아빠 어디야",
-                     "when are you coming" => "언재와",
-                     "where are you going" => "어디가",
-                     "what are you doing" => "뭐해",
-                     "what is it" => "뭐야",
-                     "i don't know" => "몰리",
                      "no" => "아니",
                      "i love you" => "사랑해"
                     }
