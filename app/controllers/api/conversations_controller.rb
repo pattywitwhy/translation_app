2db0d677a520fe92b40991bac334404e5fb80217
pattywitwhy <pattyxhwang@gmail.com>
2019-03-18 09:26:43 -0500

    conversations displaying depending on current user

--- app/controllers/api/conversations_controller.rb
+++ app/controllers/api/conversations_controller.rb
@@ -1,8 +1,11 @@
 class Api::ConversationsController < ApplicationController
+  before_action :authenticate_user, except: [:index, :show]
+
   def index
     # @conversations = current_user.conversations
     # @started_conversations = current_user.started_conversations
-    @conversations = Conversation.all
+    @conversations = Conversation.where(starter_id: current_user.id)
+    #this cannot be removed, it breaks the conversation index page
     render 'index.json.jbuilder'
   end
 
@@ -20,16 +23,21 @@ class Api::ConversationsController < ApplicationController
 
   def show
     @conversation = Conversation.find(params[:id])
-    render 'show.json.jbuilder'
+
+    if @conversation.starter_id == current_user.id
+      render 'show.json.jbuilder'
+    else
+      render json: {}, status: :unauthorized
+    end
   end
 
   def update
     @conversation = Conversation.find(params[:id])
 
-    @conversation.starter_id = params[:starter_id] || @conversation.starter_id
+    # @conversation.starter_id = params[:starter_id] || @conversation.starter_id
     @conversation.name = params[:name] || @conversation.name
 
-    if @conversation.save
+    if @conversation.starter_id == current_user.id && @conversation.save
       render 'show.json.jbuilder'
     else
       render json: { errors: @conversation.errors.full_messages }, status: :unprocessable_entity
@@ -38,9 +46,12 @@ class Api::ConversationsController < ApplicationController
 
   def destroy
     conversation = Conversation.find(params[:id])
-    conversation.destroy
 
-    render json: { message: "Successfully removed conversation" }
+    if conversation.starter_id == current_user.id
+      conversation.destroy
+      render json: { message: "Successfully removed conversation" }
+    else
+      render json: {}, status: :unauthorized
+    end
   end
-
 end
--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,19 +1,19 @@
 class Api::MessagesController < ApplicationController
-  before_action :authenticate_user
+  # before_action :authenticate_user, except: [:index, :show]
 
   def index
-    @messages = Message.all
+    # @messages = Message.all
+    @messages = current_user.messages 
     render 'index.json.jbuilder'
   end
 
   def create
     @message = Message.new(
-                            # user_id: current_user.id,
-                            user_id: params[:user_id],
+                            user_id: current_user.id,
+                            # user_id: params[:user_id],
                             conversation_id: params[:conversation_id],
                             body: params[:body]
                           )
-
     @message.translate
     
     if @message.save
@@ -25,13 +25,18 @@ class Api::MessagesController < ApplicationController
 
   def show
     @message = Message.find(params[:id])
-    render 'show.json.jbuilder'
+
+    # if @message.user_id == current_user.id
+      render 'show.json.jbuilder'
+    # else
+    #   render json: {}, status: :unauthorized
+    # end
   end
 
   def update
     @message = Message.find(params[:id])
 
-    @message.user_id = params[:user_id] || @message.user_id
+    # @message.user_id = params[:user_id] || @message.user_id
     @message.conversation_id = params[:conversation_id] || @message.conversation_id
     @message.body = params[:body] || @message.body
 
@@ -40,12 +45,26 @@ class Api::MessagesController < ApplicationController
     else
       render json: {errors: @message.errors.full_messages}, status: :unprocessable_entity
     end
+
+
+    # if @messsge.user_id == current_user.id && @message.save
+    #   render 'show.json.jbuilder'
+    # elsif @message.user_id != current_user
+    #   render json: {}, status: :unauthorized
+    # else
+    #   render json: {errors: @message.errors.full_messages}, status: :unprocessable_entity
+    # end
   end
 
   def destroy
     message = Message.find(params[:id])
-    message.destroy
 
-    render json: {message: "Successfully removed message"}
+    if message.user_id == current_user.id
+      message.destroy
+      render json: {message: "Successfully removed message"}
+    else
+      render json: {}, status: :unauthorized
+    end
+
   end
 end
