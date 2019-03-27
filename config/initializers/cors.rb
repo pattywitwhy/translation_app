56cd1d54c978d656a1feaccafc2ae6775a6316b9
pattywitwhy <pattyxhwang@gmail.com>
2019-03-27 12:23:56 -0500

    heroku added to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'https://youthful-euler-7dd6bb.netlify.com/'
+    origins 'localhost:8080', '6d7b320b.ngrok.io'
 
     resource '*',
       headers: :any,
