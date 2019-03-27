e1d5dc879ab9242fead668a1bf5db4659846f647
pattywitwhy <pattyxhwang@gmail.com>
2019-03-27 12:25:34 -0500

    ngrok link change

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', '6d7b320b.ngrok.io'
+    origins 'localhost:8080', '2110e44c.ngrok.io'
 
     resource '*',
       headers: :any,
