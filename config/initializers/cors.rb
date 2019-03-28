7a5a397e95e34749f07f8e71cf05f63b4c575e22
pattywitwhy <pattyxhwang@gmail.com>
2019-03-28 13:00:51 -0500

    changed cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'd584a1e7.ngrok.io'
+    origins 'localhost:8080', 'd6ea6b01.ngrok.io'
 
     resource '*',
       headers: :any,
